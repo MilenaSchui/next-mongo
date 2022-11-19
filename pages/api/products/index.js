9e0db158954f99efab7401c0213d5c4f292d27d8
Milena <114745314+MilenaSchui@users.noreply.github.com>
2022-11-19 10:38:40 +0100

    did some changes

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,18 +1,44 @@
-import { products } from "../_data/products";
+import connectDB from "../_db/connect-db";
+import { Product } from "../_db/models/Products";
 
-export default function handler(req, res) {
+async function handler(req, res) {
   switch (req.method) {
     case "GET":
-      const filteredProducts = req.query.category
-        ? products.filter((product) => product.category === req.query.category)
-        : products;
-      return res.status(200).json(
-        filteredProducts.map((p) => {
-          const { detail, ...rest } = p;
-          return rest;
-        })
-      );
+      try{
+        if(req.query.category){
+          const product= await Product.find({category: req.query.category});
+          res.status(200).json(product);
+        } else if (req.query.category){
+          const product= await Product.find({name: req.query.name});
+          res.status(200).json(product);
+        } else if (req.query.category){
+          const product= await Product.find({detail: req.query.details});
+          res.status(200).json(product);
+        } else {
+        const products= await Product.find({});
+        res.status(200).json(products);
+        }
+      } catch (error) {
+        res.status(500).json({error: error.message});
+
+      } 
+    break;
+    case "POST":
+      try{
+         const newProduct= new Product[{
+          name: req.body.name,
+          category: req.body.category,
+          detail: req.body.detail,
+         }];
+         await newProduct.save(newProduct);
+        }
+        catch(error){
+        res.status(500).json({error: error.message});
+        }
+    break;
     default:
       return res.status(400).json({ error: "method not supported" });
   }
 }
+
+export default connectDB(handler);
